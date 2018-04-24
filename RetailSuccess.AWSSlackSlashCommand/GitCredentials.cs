9f00e7d1a340046ac81c4d85576f4faa8e2430b7
DESKTOP-MFPMP29\Patrick Knott <Patrickk@retailsuccess.com>
2018-04-24 10:52:28 -0500

    Changed some things in GitCredentials.

--- RetailSuccess.AWSSlackSlashCommand/GitCredentials.cs
+++ RetailSuccess.AWSSlackSlashCommand/GitCredentials.cs
@@ -4,7 +4,7 @@
     {
         //To set up GitCredentials, https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
         // click settings, click developer settings, click personal access tokens, generate new token.  
-        public static string UserName = "PatrickKnott";
+        public static string UserName = "YourName";
         public static string Password = "";
     }
 }
